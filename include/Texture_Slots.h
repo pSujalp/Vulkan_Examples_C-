8044f403182bd727a9a53830f608746c7168c7a2
pSujalp <sujalpdevarathnam@gmail.com>
2026-08-11 14:57:35 +0530

    Update Texture_Slots.h

--- include/Texture_Slots.h
+++ include/Texture_Slots.h
@@ -1,59 +1,57 @@
 #pragma once
 
-
 #include "vk_types.h"
 #include "vk_initializers.h"
 #include <unordered_map>
 
+class Texture_Slots
+{
 
+public:
+	VkDevice vkdevice;
+	DeletionQueue dq;
+	VkDescriptorPool _descriptorPool;
+	std::vector<VkDescriptorSetLayout> vkVkDescriptorSet_array;
 
-class Texture_Slots{
+	std::unordered_map<VkDescriptorSet, uint8_t> TextureDescriptor_map;
 
+	Texture_Slots(VkDevice _device, DeletionQueue deletionQ, VkDescriptorPool dp) : vkdevice(_device), dq(deletionQ), _descriptorPool(dp) {}
 
-    public:
+	Texture_Slots() {}
+	void Add(AllocatedImage _texture, int setnumber, int bind = 0)
+	{
 
-    VkDevice vkdevice;
-    DeletionQueue dq;
-    VkDescriptorPool _descriptorPool;
-    std::vector<VkDescriptorSetLayout> vkVkDescriptorSet_array;
-    std::vector<VkWriteDescriptorSet> wkWds;
-     std::unordered_map<VkDescriptorSet,uint8_t> TextureDescriptor_map;
+		VkDescriptorSetLayout _TextureSetLayout = VK_NULL_HANDLE;
+		;
+		VkSampler _TextureSampler = VK_NULL_HANDLE;
+		VkImageView _textureImageView = VK_NULL_HANDLE;
+		VkDescriptorSet TextureDescriptor = VK_NULL_HANDLE;
 
-    Texture_Slots(VkDevice _device, DeletionQueue deletionQ,VkDescriptorPool dp): vkdevice(_device), dq(deletionQ), _descriptorPool(dp){}
+		VkDescriptorSetLayoutBinding textureBind = vkinit::descriptorset_layout_binding(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT, bind);
 
-    Texture_Slots(){}
-    void Add(AllocatedImage _texture, int setnumber , int bind = 0){
+		VkDescriptorSetLayoutCreateInfo set3info = {};
+		set3info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+		set3info.bindingCount = 1;
+		set3info.flags = 0;
+		set3info.pNext = nullptr;
+		set3info.pBindings = &textureBind;
 
+		vkCreateDescriptorSetLayout(vkdevice, &set3info, nullptr, &_TextureSetLayout);
 
-    VkDescriptorSetLayout _TextureSetLayout = VK_NULL_HANDLE;;
-    VkSampler _TextureSampler = VK_NULL_HANDLE;
-    VkImageView _textureImageView = VK_NULL_HANDLE; 
-    VkDescriptorSet TextureDescriptor = VK_NULL_HANDLE;
+		VkSamplerCreateInfo samplerInfo = vkinit::sampler_create_info(VK_FILTER_NEAREST);
+		VK_CHECK(vkCreateSampler(vkdevice, &samplerInfo, nullptr, &_TextureSampler));
 
-    VkDescriptorSetLayoutBinding textureBind = vkinit::descriptorset_layout_binding(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT, bind);
-
-	VkDescriptorSetLayoutCreateInfo set3info = {};
-	set3info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-	set3info.bindingCount = 1;
-	set3info.flags = 0;
-	set3info.pNext = nullptr;
-	set3info.pBindings = &textureBind;
-
-	vkCreateDescriptorSetLayout(vkdevice, &set3info, nullptr, &_TextureSetLayout);
-
-    VkSamplerCreateInfo samplerInfo = vkinit::sampler_create_info(VK_FILTER_NEAREST);
-	VK_CHECK(vkCreateSampler(vkdevice, &samplerInfo, nullptr, &_TextureSampler));
-
-    dq.push_function([=](){ vkDestroySampler(vkdevice, _TextureSampler, nullptr);
+		dq.push_function([=]()
+						 { vkDestroySampler(vkdevice, _TextureSampler, nullptr);
 	   vkDestroyDescriptorSetLayout(vkdevice, _TextureSetLayout, nullptr); });
 
+		VkImageViewCreateInfo imageInfo = vkinit::imageview_create_info(VK_FORMAT_R8G8B8A8_SRGB, _texture.image, VK_IMAGE_ASPECT_COLOR_BIT);
+		VK_CHECK(vkCreateImageView(vkdevice, &imageInfo, nullptr, &_textureImageView));
 
-    VkImageViewCreateInfo imageInfo = vkinit::imageview_create_info(VK_FORMAT_R8G8B8A8_SRGB, _texture.image, VK_IMAGE_ASPECT_COLOR_BIT);
-	VK_CHECK(vkCreateImageView(vkdevice, &imageInfo, nullptr, &_textureImageView));
+		dq.push_function([=]()
+						 { vkDestroyImageView(vkdevice, _textureImageView, nullptr); });
 
-	dq.push_function([=](){ vkDestroyImageView(vkdevice, _textureImageView, nullptr); });
-
-    VkDescriptorSetAllocateInfo texAllocInfo = {};
+		VkDescriptorSetAllocateInfo texAllocInfo = {};
 		texAllocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
 		texAllocInfo.pNext = nullptr;
 		texAllocInfo.descriptorPool = _descriptorPool;
@@ -62,9 +60,9 @@ class Texture_Slots{
 
 		VK_CHECK(vkAllocateDescriptorSets(vkdevice, &texAllocInfo, &TextureDescriptor));
 
-        vkVkDescriptorSet_array.emplace_back(_TextureSetLayout);
+		vkVkDescriptorSet_array.emplace_back(_TextureSetLayout);
 
-    	VkDescriptorImageInfo imageBufferInfo = {};
+		VkDescriptorImageInfo imageBufferInfo = {};
 		imageBufferInfo.sampler = _TextureSampler;
 		imageBufferInfo.imageView = _textureImageView;
 		imageBufferInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
@@ -77,11 +75,9 @@ class Texture_Slots{
 		Writetexture0.descriptorCount = 1;
 		Writetexture0.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
 		Writetexture0.pImageInfo = &imageBufferInfo;
-		wkWds.emplace_back(Writetexture0);
-        TextureDescriptor_map[TextureDescriptor] = setnumber;
-		vkUpdateDescriptorSets(vkdevice,1, &Writetexture0, 0, nullptr);
-    }
 
+		TextureDescriptor_map[TextureDescriptor] = setnumber;
 
-
+		vkUpdateDescriptorSets(vkdevice, 1, &Writetexture0, 0, nullptr);
+	}
 };
